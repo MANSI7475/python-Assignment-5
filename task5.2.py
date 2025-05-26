d02c161f41e17fad6341440186e7325ec9c254c9
MANSI7475 <pandyamansi48@gmail.com>
2025-05-26 11:14:11 +0530

    Add files via upload

---
+++ task5.2.py
@@ -0,0 +1,7 @@
+num=list(range(1,11))
+print(num)
+first_five=num[:5]
+print("extract the first five element:",first_five)
+reverse_first_five=first_five[::-1]
+print("extract the reverse first five element:",reverse_first_five)
+
